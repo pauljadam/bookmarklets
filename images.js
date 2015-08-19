633a40254225de26d918ddcbc9e914e3e1f0a71b
Paul J. Adam <paul@pauljadam.com>
2015-08-19 18:52:17 -0500

    images

---
+++ images.js
@@ -0,0 +1,19 @@
+javascript: (function() {
+    function callback() {
+        function l() {
+        $("span").remove(".altSpan");
+		$("img").each(function() {
+			if (!this.hasAttribute('alt')) {
+				$(this).attr('style','border:red 2px solid;padding:2px;');
+   			 	$( this ).before("<span class=\"altSpan\" style=\"border:red 2px solid;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:absolute;line-height:100%;z-index:2;\">alt=\""+$(this).attr('alt')+"\"</span>");
+			} else {
+				$(this).attr('style','border:green 2px solid;padding:2px;');
+   			 	$( this ).before("<span class=\"altSpan\" style=\"border:green 2px solid;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:absolute;line-height:100%;z-index:2\">alt=\""+$(this).attr('alt')+"\"</span>");
+			}
+	    });
+        }
+        l()
+    }
+    var s = document.createElement("script");
+    s.addEventListener ? s.addEventListener("load", callback, !1) : s.readyState && (s.onreadystatechange = callback), s.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js", document.body.appendChild(s);
+})()
