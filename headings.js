0c7ce4ef4e0a66a515220001a2b7872f78b9da7d
Paul J. Adam <paul@pauljadam.com>
2015-08-27 22:36:54 -0500

    alert on aria headings

--- headings.js
+++ headings.js
@@ -27,8 +27,8 @@ javascript: (function() {
         $("[role=heading][aria-level=5]").append("<strong class=\"closeSpan\" style=\"color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow\">&lt;/[role=heading][aria-level=5]&gt;</strong>");
         $("[role=heading][aria-level=6]").prepend("<strong class=\"openSpan\" style=\"color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow\">&lt;[role=heading][aria-level=6]&gt;</strong>");
         $("[role=heading][aria-level=6]").append("<strong class=\"closeSpan\" style=\"color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow\">&lt;/[role=heading][aria-level=6]&gt;</strong>");
-			if (!$('h1').length && !$('h2').length && !$('h3').length && !$('h4').length && !$('h5').length && !$('h6').length) {
-			alert('No Headings Level 1-6 Found!');
+			if (!$('h1').length && !$('h2').length && !$('h3').length && !$('h4').length && !$('h5').length && !$('h6').length && !$('[role=heading][aria-level=1]').length && !$('[role=heading][aria-level=2]').length && !$('[role=heading][aria-level=3]').length && !$('[role=heading][aria-level=4]').length && !$('[role=heading][aria-level=5]').length && !$('[role=heading][aria-level=6]').length) {
+			alert('No Headings Level 1-6 or WAI-ARIA role=heading Elements Found!');
 			} else {
 				console.log('headings found');
 				$('body').append('<div id="success" role="alert"></div>');
