2236502bce4782589c4dc9ffaf2a04e211e70c8c
Paul J. Adam <paul@pauljadam.com>
2015-08-27 23:49:50 -0500

    css3 speech

--- images.js
+++ images.js
@@ -9,16 +9,15 @@ javascript: (function() {
    			 	$(this).before("<span class=\"altSpan\" style=\"border:red 2px solid;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:absolute;line-height:100%;z-index:2147483647;\">NO ALT</span>");
 			} else {
 				$(this).attr('style','border:green 2px solid;padding:2px;');
-   			 	$(this).before("<span class=\"altSpan\" style=\"border:green 2px solid;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:absolute;line-height:100%;z-index:2147483647;\">alt=\""+$(this).attr('alt')+"\"</span>");
+   			 	$(this).before("<span class=\"altSpan\" style=\"border:green 2px solid;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:absolute;line-height:100%;z-index:2147483647;speak: literal-punctuation;\">alt=\""+$(this).attr('alt')+"\"</span>");
 			}
 	    });
-					if (!$('img').length) {
-			alert('No <IMG> Elements Found!');
+			if (!$('img').length) {
+				alert('No <IMG> Elements Found!');
 			} else {
 				$('body').append('<div id="success" role="alert" style="position:absolute; width:0; height:0; clip: rect(0,0,0,0);"></div>');
 				$('#success').html('Success! Images Found!');
 			}
-
         }
         l()
     }
