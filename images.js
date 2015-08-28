dff03562aa973cf4ae5e39ed49db9ed97330b156
Paul J. Adam <paul@pauljadam.com>
2015-08-27 23:23:47 -0500

    n

--- images.js
+++ images.js
@@ -13,7 +13,7 @@ javascript: (function() {
 			}
 	    });
 					if (!$('img').length) {
-			alert('No <img> Elements Found!');
+			alert('No <IMG> Elements Found!');
 			} else {
 				$('body').append('<div id="success" role="alert" style="position:absolute; width:0; height:0; clip: rect(0,0,0,0);"></div>');
 				$('#success').html('Success! Images Found!');
