f0e0f319e7b64c203be5cbab1008bfd2f3dfbaa7
Paul J. Adam <paul@pauljadam.com>
2015-11-11 18:44:20 -0600

    title

--- tabindex.js
+++ tabindex.js
@@ -6,7 +6,7 @@ javascript: (function() {
 		$("*").each(function() {
 			if (this.hasAttribute('tabindex')) {
 				$(this).attr('style','outline:orange 2px dashed;padding:2px;');
-   			 	$(this).after("<span class=\"tabindexSpan\" style=\"outline:orange 2px dashed;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:relative;line-height:100%;z-index:2147483647;\">tabindex=\""+$(this).attr('tabindex')+"\"</span>");
+   			 	$(this).after("<span role=\"region\" aria-label=\"Tabindex\" class=\"tabindexSpan\" style=\"outline:orange 2px dashed;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:relative;line-height:100%;z-index:2147483647;\">tabindex=\""+$(this).attr('tabindex')+"\"</span>");
 				}
 	    });
 			if (!$('[tabindex]').length) {
---
+++ title.js
@@ -0,0 +1,23 @@
+javascript: (function() {
+    function callback() {
+        function l() {
+        $("span").remove(".axSpan");
+		$('#success').remove();
+		$("*").each(function() {
+			if (this.hasAttribute('title')) {
+				$(this).attr('style','outline:red 1px solid;padding:2px;');
+   			 	$(this).after("<span role=\"region\" aria-label=\"Title\" class=\"axSpan\" style=\"outline:red 1px solid;padding:1px;color:black;font-family:sans-serif;font-weight:bold;font-size:small;background-color:yellow;position:relative;line-height:100%;z-index:2147483647;\">title=\""+$(this).attr('title')+"\"</span>");
+				}
+	    });
+			if (!$('[title]').length) {
+				alert('No <HTML> Elements with title attributes Found!');
+			} else {
+				$('body').append('<div id="success" role="alert" style="position:absolute; width:0; height:0; clip: rect(0,0,0,0);"></div>');
+				$('#success').html('Success! title attributes Found!');
+			}
+        }
+        l()
+    }
+    var s = document.createElement("script");
+    s.addEventListener ? s.addEventListener("load", callback, !1) : s.readyState && (s.onreadystatechange = callback), s.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js", document.body.appendChild(s);
+})()
