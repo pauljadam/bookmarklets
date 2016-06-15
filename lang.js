e3c76de5257b3a838d9be7097c0048724b587996
Paul J. Adam <paul@pauljadam.com>
2016-06-15 15:55:36 -0500

    else ifs

--- lang.js
+++ lang.js
@@ -1,6 +1,9 @@
-if(document.documentElement.lang) {
-	alert('Page Titled: {' + document.title + '} Has documentElement.lang attribute lang=\"' + document.documentElement.lang + '\"');
-} 
-if(document.getElementsByTagName('html')[0].getAttribute('xml:lang')) {
-	alert('Page Titled: {' + document.title + '} Has attribute xml:lang=\"' + document.getElementsByTagName('html')[0].getAttribute('xml:lang') + '\"');
-} 
+if(document.getElementsByTagName('html')[0].getAttribute('xml:lang') && document.documentElement.lang) {
+	alert('Page <title>: {' + document.title + '} Has documentElement.lang attribute lang=\"' + document.documentElement.lang + '\" AND Has attribute xml:lang=\"' + document.getElementsByTagName('html')[0].getAttribute('xml:lang') + '\"');
+} else if(document.documentElement.lang) {
+	alert('Page <title>: {' + document.title + '} Has documentElement.lang attribute lang=\"' + document.documentElement.lang + '\"');
+} else if(document.getElementsByTagName('html')[0].getAttribute('xml:lang')) {
+	alert('Page <title>: {' + document.title + '} Has attribute xml:lang=\"' + document.getElementsByTagName('html')[0].getAttribute('xml:lang') + '\"');
+} else {
+		alert('Page <title>: {' + document.title + '} Has no lang or xml:lang attribute found!');
+}
