d89999ad3df8db3361040619577a9ce434b9d2c7
Paul J. Adam <paul@pauljadam.com>
2016-06-15 15:41:05 -0500

    xml:lang added

--- lang.js
+++ lang.js
@@ -1,1 +1,4 @@
-alert('Page Titled: {' + document.title + '} Has Language Set: {' + document.documentElement.lang + '}');
+alert('Page Titled: {' + document.title + '} Has document.title Set: lang=\"' + document.documentElement.lang + '\"');
+if(document.getElementsByTagName('html')[0].getAttribute('xml:lang')) {
+	alert('Page Titled: {' + document.title + '} Has xml:lang=\"' + document.getElementsByTagName('html')[0].getAttribute('xml:lang') + '\"');
+} 
