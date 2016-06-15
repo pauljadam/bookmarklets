9a2d1f3380e961bd6ae5a0757deb00c82104e83f
Paul J. Adam <paul@pauljadam.com>
2016-06-15 15:46:13 -0500

    bugs

--- lang.js
+++ lang.js
@@ -1,6 +1,6 @@
 if(document.documentElement.lang) {
-	alert('Page Titled: {' + document.title + '} Has document.title Set: lang=\"' + document.documentElement.lang + '\"');
+	alert('Page Titled: {' + document.title + '} Has documentElement.lang attribute lang=\"' + document.documentElement.lang + '\"');
 } 
 if(document.getElementsByTagName('html')[0].getAttribute('xml:lang')) {
-	alert('Page Titled: {' + document.title + '} Has xml:lang=\"' + document.getElementsByTagName('html')[0].getAttribute('xml:lang') + '\"');
+	alert('Page Titled: {' + document.title + '} Has attribute xml:lang=\"' + document.getElementsByTagName('html')[0].getAttribute('xml:lang') + '\"');
 } 
