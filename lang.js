03b61244da5c469df2a198ac42df2451e8e4bcbc
Paul J. Adam <paul@pauljadam.com>
2016-06-15 15:42:37 -0500

    if

--- lang.js
+++ lang.js
@@ -1,4 +1,6 @@
-alert('Page Titled: {' + document.title + '} Has document.title Set: lang=\"' + document.documentElement.lang + '\"');
+if(document.title) {
+	alert('Page Titled: {' + document.title + '} Has document.title Set: lang=\"' + document.documentElement.lang + '\"');
+} 
 if(document.getElementsByTagName('html')[0].getAttribute('xml:lang')) {
 	alert('Page Titled: {' + document.title + '} Has xml:lang=\"' + document.getElementsByTagName('html')[0].getAttribute('xml:lang') + '\"');
 } 
