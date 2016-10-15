7d9b0bc26fab0c069d6f919792b022e260d2f7a4
Paul J. Adam <paul@pauljadam.com>
2016-10-14 23:57:02 -0500

    axe

---
+++ axe.js
@@ -0,0 +1,9 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/axe-core/2.0.7/axe.js';
+document.head.appendChild(script);
+setTimeout(function(){
+axe.a11yCheck(document, function(results) {
+  console.log(results);
+});
+}, 100);
